7fdfc2306f79f32511e9150023a260c9e4af8f0a
sj800 <surajshukla800@gmail.com>
2021-06-08 03:29:27 +0530

    add

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -1,2 +1,10 @@
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class Application {
+
+    public static void main(String[] args) {
+
+    }
+
 }
